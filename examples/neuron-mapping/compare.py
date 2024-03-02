f801444c298e7b2583d4f976701dc9532a0b143a
TetraspaceW <holomanga@gmail.com>
2024-03-02 18:27:05 +0000

    plot the cdf for the overlap between the neurons

--- examples/neuron-mapping/compare.py
+++ examples/neuron-mapping/compare.py
@@ -50,6 +50,37 @@ def compare_pruned_ff_criteria(
     return ratios
 
 
+def get_grid(
+    cripple_repos: list[str],
+    model_size: str,
+    path: str = "/home/ubuntu/taker-rashid/examples/neuron-mapping/saved_tensors/",
+    focus_repo: str = "pile",
+    prune_ratio: float = 0.01,
+):
+    comparison = compare_pruned_ff_criteria(
+        cripple_repos,
+        model_size,
+        path=path,
+        focus_repo=focus_repo,
+        prune_ratio=prune_ratio,
+    )
+
+    grid = [
+        [
+            (
+                comparison[dataset_a][dataset_b].item()
+                if dataset_a != dataset_b
+                else np.nan
+            )
+            for dataset_b in datasets
+        ]
+        for dataset_a in datasets
+    ]
+    grid: np.ndarray = np.ma.masked_where(np.isnan(grid), grid)
+
+    return grid
+
+
 datasets = [
     f"cifar20-{dataset}"
     for dataset in [
@@ -75,9 +106,17 @@ datasets = [
     ]
 ]
 
-prune_ratio = 0.01
+prune_ratio = 0.05
 
-comparison = compare_pruned_ff_criteria(
+grid = get_grid(
+    datasets,
+    "Cifar100",
+    path="examples/neuron-mapping/saved_tensors/",
+    focus_repo="cifar20-split",
+    prune_ratio=prune_ratio,
+)
+
+grid_random = get_grid(
     datasets,
     "init",
     path="examples/neuron-mapping/saved_tensors/",
@@ -85,19 +124,39 @@ comparison = compare_pruned_ff_criteria(
     prune_ratio=prune_ratio,
 )
 
-grid = [
-    [
-        comparison[dataset_a][dataset_b].item() if dataset_a != dataset_b else np.nan
-        for dataset_b in datasets
-    ]
-    for dataset_a in datasets
-]
-grid = np.ma.masked_where(np.isnan(grid), grid)
-
+# get median across non nan value
 average = np.mean(grid)
 
 # reset all plot settings from configuration in taker proper
 plt.rcdefaults()
+
+
+def plot_overlap_cdf(
+    grid1, grid2, grid1_label="Pretrained model", grid2_label="Random init"
+):
+    y_trained = np.searchsorted(np.sort(grid1.flatten()), grid1.flatten()) / len(
+        grid1.flatten()
+    )
+    y_trained.sort()
+    y_random = np.searchsorted(
+        np.sort(grid2.flatten()), grid2.flatten()
+    ) / len(grid2.flatten())
+    y_random.sort()
+    x_trained = grid1.flatten()
+    x_trained.sort()
+    x_random = grid2.flatten()
+    x_random.sort()
+    plt.plot(x_trained, y_trained, "-r", label=grid1_label)
+    plt.plot(x_random, y_random, "-b", label=grid2_label)
+    plt.xlabel("Overlap in neurons pruned")
+    plt.ylabel("Cumulative frequency of overlap")
+    plt.title("Distribution of overlap in neurons pruned, prune_ratio = 0.05")
+    plt.legend()
+
+    plt.show()
+
+
+plot_overlap_cdf(grid, grid_random)
 
 plt.imshow(grid)
 for i in range(len(datasets)):
